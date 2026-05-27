2c8a69d11f9fd75ba3e3fec1ddf85321c5052eba
rotatingsandwiches2 <99811015+rotatingsandwiches2@users.noreply.github.com>
2026-05-27 11:54:29 -0400

    Update Mod.lua

--- Mod.lua
+++ Mod.lua
@@ -20,7 +20,7 @@ SMODS.Joker {
     name = 'BING BONG',
     -- The description of the card
     text = {
-      "{C:mult}+20 {} Mult",
+      "{C:mult} +#1# {} Mult",
       "{C:green}1 in 3 {} chance this",
       "card goes bye bye",
       "at end of round"
@@ -29,5 +29,17 @@ SMODS.Joker {
   },
   -- Config I think
   config = { extra = { mult = 4} },
+  -- GIMME THOSE VARIABLES
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.mult } }
+  end,
+  -- The boi's rarity
+  rarity = 1,
+  -- YO WHERES UR ATLAS?
+  atlas = 'Gaming',
+  -- Where it be on the collection
+  pos = { x = 0, y = 0 },
+  -- Moneydollarbucks?
+  cost = 1,
   
 }
