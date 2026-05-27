ec04978e87b2b6734f9322d3d4e5864ffc8884ab
rotatingsandwiches2 <99811015+rotatingsandwiches2@users.noreply.github.com>
2026-05-27 11:06:09 -0400

    Update Mod.lua

--- Mod.lua
+++ Mod.lua
@@ -1,1 +1,33 @@
-SMODS.Atlas {}
+SMODS.Atlas {
+  -- Key to associate cards with
+  key = "Gaming",
+  -- Atlas sprite path
+  path = "Gaming.png",
+  -- Pixel on X
+  px = 71,
+  -- Pixel on Y
+  py = 95,
+}
+
+
+-- Bing Bong
+SMODS.Joker {
+  -- The joker's key
+  key = 'bingbong',
+  -- What shows in game
+  loc_txt = {
+    -- The card's name in game
+    name = 'BING BONG',
+    -- The description of the card
+    text = {
+      "{C:mult}+20 {} Mult",
+      "{C:green}1 in 3 {} chance this",
+      "card goes bye bye",
+      "at end of round"
+    }
+    
+  },
+  -- Config I think
+  config = { extra = { mult = 4} },
+  
+}
